3f6d822aa3358b3d2f89dcb6bcf0bd9a6647e926
Brayanj732 <brayjimenezorozco@hotmail.com>
2025-12-30 02:42:52 -0500

    chore: initial commit - ASP.NET Core MVC inventory project setup

---
+++ Project2/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace Project2.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ItemClient>? ItemClients { get; set; }
+    }
+}
